414771d36d65f2415f0f7489c9ffdb3b34629449
Roberto162 <l23201253@pachuca.tecnm.mx>
2026-04-12 09:28:46 -0600

    Creacion de pantalla de inicio, con fondo

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+const canvas = document.getElementById("canvasJuego");
+const ctx = canvas.getContext("2d");
+
+let score = 0;
+let salud = 100;
+let record = 0;
+
+const textoIntro = document.getElementById("textoIntro");
+const mensajeFinal = document.getElementById("mensajeFinal");
+const btnComenzar = document.getElementById("btnComenzar");
+const pantallaInicio = document.getElementById("pantallaInicio");
+const sonidoIntro = document.getElementById("sonidoIntro");
+
+const texto = `Tu misión es limpiar la ciudad.
+
+Haz clic en la basura para ganar puntos.
+⚠️ Evita los vidrios o perderás salud.
+❤️ Encuentra botiquines para recuperarte.
+
+Si tu salud llega a 0... el mundo pierde.`;
+
+// EFECTO MAQUINA DE ESCRIBIR
+let i = 0;
+
+function escribirTexto() {
+    if (i < texto.length) {
+        textoIntro.innerHTML += texto.charAt(i);
+        i++;
+        setTimeout(escribirTexto, 30);
+    } else {
+        mostrarMensajeFinal();
+    }
+}
+
+// MOSTRAR MENSAJE FINAL
+function mostrarMensajeFinal() {
+    setTimeout(() => {
+        mensajeFinal.classList.add("visible");
+        sonidoIntro.play(); // sonido retro 🔊
+    }, 1000);
+}
+
+// INICIAR INTRO
+window.onload = () => {
+    escribirTexto();
+};
+
+// BOTON COMENZAR
+btnComenzar.addEventListener("click", () => {
+
+    // Fade out
+    pantallaInicio.classList.add("fade-out");
+
+    setTimeout(() => {
+        pantallaInicio.style.display = "none";
+        iniciarJuego();
+    }, 1000);
+});
+
+// FUNCION BASE DEL JUEGO
+function iniciarJuego() {
+    console.log("Juego iniciado");
+}
